0575e1f7a88409a188e3b161399e15a8cbf928b8
mattew8 <mattew4483@likelion.org>
2021-02-14 13:42:14 +0900

    [add] 같은 숫자는 싫어

---
+++ Level 1/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer_num = 0
+    answer = [arr[0]] 
+    for i in range(len(arr)):
+        if arr[i] != answer[answer_num]:
+            answer.append(arr[i])
+            answer_num += 1
+    return answer
+
+print(solution([1,1,2,3,4,4]))
+# [1,2,3,4]
